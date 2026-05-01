aad4ee1d050c5cb38dfed553ceb5baf1f200c894
PA-RSS <preston@retailservicesystems.com>
2026-05-01 10:45:56 -0400

    Initialize Electron app with main window setup

---
+++ main.js
@@ -0,0 +1,29 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 600,
+    height: 700,
+    minWidth: 460,
+    minHeight: 400,
+    title: 'TDL',
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true,
+    },
+  });
+
+  win.loadFile('index.html');
+  win.setMenuBarVisibility(false);
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  app.quit();
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) createWindow();
+});
